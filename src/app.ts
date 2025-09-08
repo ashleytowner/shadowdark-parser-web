2a142a170b78fcd1fa186a2797777e633d316789
Ashley Towner <git@ashtowner.com>
2025-09-08 17:19:13 +0930

    fix: Doesn't crash on error

--- src/app.ts
+++ src/app.ts
@@ -21,25 +21,35 @@ const upload = multer({ dest: path.join(__dirname, "uploaded-templates/") });
 app.post("/api/parse", upload.single("template"), (req, res) => {
   const { data, amount } = req.body;
   let result: ReturnType<typeof parse> | ReturnType<typeof bulkParse>;
-  switch (amount) {
-    case "single":
-      result = parse(data);
-      break;
-    default:
-      result = bulkParse(data, allCapsStrategy);
-  }
+  try {
+    switch (amount) {
+      case "single":
+        result = parse(data);
+        break;
+      default:
+        result = bulkParse(data, allCapsStrategy);
+    }
 
-  if (req.file) {
-    const template = getTemplateFromFile(req.file.path);
-    res.type('text/plain').send(template(result));
-    unlink(req.file.path, (err) => {
-      if (err) {
-        console.error(err);
-      }
-      console.log("Removed file");
-    });
-  } else {
-    res.send(result);
+    if (req.file) {
+      const template = getTemplateFromFile(req.file.path);
+      res.type("text/plain").send(template(result));
+      unlink(req.file.path, (err) => {
+        if (err) {
+          console.error(err);
+        }
+        console.log("Removed file");
+      });
+    } else {
+      res.send(result);
+    }
+  } catch (e: unknown) {
+    console.error("Error!");
+    if (e instanceof Error) {
+      console.error(e.message);
+    } else {
+      console.error(e);
+    }
+    res.status(500).send("Parsing error");
   }
 });
 
